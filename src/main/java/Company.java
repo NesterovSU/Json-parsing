b4b1f35e7fd0ddbb5860a4bcef48f32979392d3b
Sergey Nesterov <nesterov_s_u@mail.ru>
2022-03-01 21:00:59 +0300

    Filter securities by input date

--- src/main/java/Company.java
+++ src/main/java/Company.java
@@ -1,16 +1,19 @@
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.*;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
 /**
  * @author Sergey Nesterov
  */
-@Setter
+
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 public class Company {
     private int id;
@@ -18,22 +21,21 @@ public class Company {
                     address,
                     phoneNumber,
                     inn;
-    @JsonFormat(shape = JsonFormat.Shape.STRING,
-            pattern = "dd.MM.yyyy")
-    private Date founded;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate founded;
     private List<Security> securities;
 
-    @Setter
     @Getter
+    @Setter
     @NoArgsConstructor
-    @AllArgsConstructor
-    @ToString
-    private static class Security{
+    @ToString()
+    static class Security{
         private String  name,
                         code;
-        @JsonFormat(shape = JsonFormat.Shape.STRING,
-                pattern = "dd.MM.yyyy")
-        private Date date;
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        private LocalDate date;
         private List<String> currency;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,63 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Sergey Nesterov
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         File file = new File("shares.json");
         ObjectMapper mapper = new ObjectMapper();
         Companies companies = mapper.readValue(file, Companies.class);
-        companies.getCompanies().forEach(System.out::println);
-        System.out.println(companies.getCompanies().get(1).getSecurities().get(1));
+
+        //Вывести все имеющиеся компании в формате «Краткое название» – «Дата основания 17/01/98»;
+        System.out.println("Все имеющиеся компании:");
+        final DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd/MM/yy");
+        companies.getCompanies().forEach(c -> System.out.printf("%s - %s\n",
+                        c.getName(),
+                        c.getFounded().format(f1)));
+
+        //Вывести все ценные бумаги (их код, дату истечения и полное название организации-владельца),
+        //которые просрочены на текущий день, а также посчитать суммарное число всех таких бумаг;
+        LocalDate today = LocalDate.now();
+        AtomicInteger count = new AtomicInteger(0);
+        System.out.println("\nПросроченные бумаги:");
+        companies.getCompanies().stream().map(c->c.getSecurities().stream().filter(s-> s.getDate().isBefore(today)))
+                .forEach(l->l.forEach((s)->{
+                    System.out.printf("код = %s, дата истечения = %s, %s\n",
+                            s.getCode(),
+                            s.getDate().format(f1),
+                            s.getName());
+                    count.getAndSet(count.get() + 1);
+                }));
+        System.out.printf("Количество бумаг = %d\n", count.get());
+
+        // На запрос пользователя в виде даты «ДД.ММ.ГГГГ», «ДД.ММ,ГГ», «ДД/ММ/ГГГГ» и «ДД/ММ/ГГ»
+        // вывести название и дату создания всех организаций, основанных после введенной даты;
+        System.out.println("Введите запрос в виде даты:");
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNext()) input = scanner.nextLine();
+        String pattern = input.replaceFirst("\\d{2}", "dd")
+                .replaceFirst("\\d{2}", "MM")
+                .replaceFirst("\\d{2}", "yy")
+                .replaceFirst("\\d{2}", "yy");
+        DateTimeFormatter f = DateTimeFormatter.ofPattern(pattern);
+        LocalDate inputDate = LocalDate.parse(input, f);
+        if(inputDate.getYear()>2069)
+            inputDate = inputDate.minusYears(100);
+        final LocalDate iDate= inputDate;
+        System.out.println("Организации основанные позднее:");
+        companies.getCompanies().stream().filter(c->c.getFounded().isAfter(iDate))
+                .forEach(c-> System.out.printf("%s - %s\n",
+                        c.getName(),
+                        c.getFounded().format(f)));
     }
 }
